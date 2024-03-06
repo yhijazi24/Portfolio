5f9c680cf9c3d0e3072df7aab0b34773f2cba696
yhijazi24 <yahyahijazi643@gmail.com>
2024-03-06 13:40:08 +0100

    push

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
+window.addEventListener('scroll', function() {
+    var header = document.getElementById('header');
+    if (window.scrollY > 100) {
+        header.style.backgroundColor = '#a2161adb';
+    } else {
+        header.style.backgroundColor = 'transparent';
+    }
+});
+
 /*=============== Hamburger Menu ===============*/
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".navbar");
@@ -29,41 +38,42 @@ navLinks.forEach((link) => {
 const imageUrls0 = ['img/project1_img/ebook2.png', 'img/project1_img/ebook3.png', 'img/project1_img/ebook4.png'];
 
 let index0 = 0;
-function changeImage() {
+function changeImages0() {
     document.getElementById('image1').src = imageUrls0[index0];
     index0 = (index0 + 1) % imageUrls0.length;
 }
 
-setInterval(changeImage, 3000);
+setInterval(changeImages0, 3000);
 
 
 const imageUrls1 = ['img/project2_img/mustad2.png', 'img/project2_img/mustad3.png', 'img/project2_img/mustad4.png', 'img/project2_img/mustad5.png', 'img/project2_img/mustad6.png', 'img/project2_img/mustad7.png', 'img/project2_img/mustad8.png', 'img/project2_img/mustad9.png', 'img/project2_img/mustad10.png', 'img/project2_img/mustad11.png', 'img/project2_img/mustad12.png', 'img/project2_img/mustad13.png'];
 
 let index1 = 0;
-function changeImages() {
+function changeImages1() {
     document.getElementById('image2').src = imageUrls1[index1];
     index1 = (index1 + 1) % imageUrls1.length;
 }
+setInterval(changeImages1, 3000);
 
 const imageUrls2 = ['img/project3_img/portfolio2.PNG', 'img/project3_img/portfolio3.PNG', 'img/project3_img/portfolio4.PNG'];
 
 let index2 = 0;
-function changeImages() {
+function changeImages2() {
     document.getElementById('image3').src = imageUrls2[index2];
     index2 = (index2 + 1) % imageUrls2.length;
 }
 
-setInterval(changeImages, 3000);
-img/project5__img/init1.PNG
+setInterval(changeImages2, 3000);
+
 const imageUrls3 = ['img/project5__img/init1.PNG', 'img/project5__img/init2.PNG', 'img/project5__img/init3.PNG', 'img/project5__img/init4.PNG'];
 
 let index3 = 0;
-function changeImages() {
-    document.getElementById('image3').src = imageUrls3[index3];
+function changeImages3() {
+    document.getElementById('image5').src = imageUrls3[index3];
     index3 = (index3 + 1) % imageUrls3.length;
 }
 
-setInterval(changeImages, 3000);
+setInterval(changeImages3, 3000);
 
 /*=============== View More and Hide Feature ===============*/
 document.addEventListener("DOMContentLoaded", function () {
